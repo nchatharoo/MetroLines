280b5096941e2cc2e50f0bd4ce4e1a848a39c2e4
Nadheer Chatharoo <nchatharoo@icloud.com>
2020-06-30 10:57:22 +0200

    Initial Commit

---
+++ MetroLines/MetroLinesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetroLinesApp.swift
+//  MetroLines
+//
+//  Created by Chatharoo on 30/06/2020.
+//
+
+import SwiftUI
+
+@main
+struct MetroLinesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
